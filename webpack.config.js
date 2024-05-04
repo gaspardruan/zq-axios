7bfa4195515b0c69b1a34dc00ca2a8c2aa78aa6d
GaspardRuan <1039553124@qq.com>
2024-05-04 19:54:49 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.ts'),
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'zq-axios',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.ts'],
+  }
+};
